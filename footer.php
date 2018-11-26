9f77d807857fd56daeab1a9aa8bdcd1c70418873
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-11-26 19:42:06 +0000

    initial work

--- footer.php
+++ footer.php
@@ -1,6 +1,76 @@
 <footer class="to_global-footer">
-    <div class="to_wrap">
-        Footer
+    <div class="to_footer-media-partners">
+        <div class="to_wrap">
+            <h3>Media Partners</h3>
+            <ul>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="to_footer-quick-links">
+        <div class="to_wrap">
+            <ul>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+                <li><a hred="#">Link Item</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="to_footer-sub-footer">
+        <div class="to_wrap">
+            <div class="to_sub-footer">
+                Footer Section One
+            </div>
+            <div class="to_sub-footer">
+                Footer Section Two
+            </div>
+            <div class="to_sub-footer">
+                Footer Section Three
+            </div>
+        </div>
+    </div>
+    <div class="to_footer-websites">
+        <div class="to_wrap">
+            <ul>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+                <li><a hred="#"><img src="#"></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="to_global-credits">
+        <div class="to_wrap">
+            <p>Touch Medical Media Limited, trading as Touch Medical Media, is a private limited company registered in England and Wales at The White House, Mill Road, Goring, Reading, England, RG8 9DD with registered number 08197142. © 2018 All rights reserved.</p>
+            <nav>
+                <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Cookies Policy</a></li>
+                    <li><a href="#">Website Use</a></li>
+                    <li><a href="#">Terms and Conditions</a></li>
+                </ul>
+            </nav>
+            <p>Touch Oncology is for informational purposes and should not be considered medical advice, diagnosis or treatment recommendations.</p>
+        </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -9,17 +9,53 @@
     <div class="to_header-bar">
         <ul><?php wp_nav_menu( array('theme_location' => 'header_bar' )); ?></ul>
     </div>
+    <div class="to_header-ad">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/ad-placeholder.jpg" alt="test">
+    </div>
 	<header class="to_global-header">
 		<div class="to_wrap">
-			<h1 class="sbh_global-logo">
+			<h1 class="to_global-logo">
 				<a href="<?php bloginfo('url'); ?>">
-
+                    Logo
 				</a>
 			</h1>
-			<nav class="sbh_global-nav">
+			<nav class="to_global-nav">
 				<ul>
 					<?php wp_nav_menu( array('theme_location' => 'main_menu' )); ?>
 				</ul>
 			</nav>
 		</div>
-	</header>
+    </header>
+    <div class="to_global-filters">
+        <div class="to_global-filter-title">
+            Filters
+        </div>
+        <nav class="to_global-filter-title">
+            <ul>
+                <li><a href="#">Breast Cancer</a></li>
+                <li><a href="#">Immunotherapy</a></li>
+                <li><a href="#">Lung Cancer</a></li>
+                <li><a href="#">Genitourianry Cancer</a></li>
+                <li><a href="#">Gastrointenstinal Cancer</a></li>
+                <li><a href="#">Haematological</a></li>
+                <li><a href="#">Malignancies</a></li>
+                <li><a href="#">Radiology</a></li>
+                <li><a href="#">Gynaecological Cancers</a></li>
+                <li><a href="#">Dermatoloical Cancer</a></li>
+                <li><a href="#">Diagnostics and Screening</a></li>
+                <li><a href="#">Haematology</a></li>
+                <li><a href="#">Thyroid Cancer</a></li>
+                <li><a href="#">Radiotherapy</a></li>
+                <li><a href="#">View All</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="to_global-search">
+        <div class="to_global-search-wrap">
+            <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+				<label for="s">Search</label>
+					<input  placeholder="Search" type="search" value="Search" name="s" id="s" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+					<button id="searchsubmit">Search</button>
+                </form>
+        </div>
+    </div>
